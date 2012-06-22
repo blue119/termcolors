ac06af1a13826bff13ee99ede5f8ed6aa902ddb5
Yao-Po Wang <blue119@gmail.com>
2012-06-22 14:02:17 +0800

    add some comments

--- termcolors/__init__.py
+++ termcolors/__init__.py
@@ -1,10 +1,14 @@
 # Foreground colors
-
 tf = ['black', 'red', 'green', 'yellow', 'blue', 'pink', 'cyan', 'white']
+
+# Foreground hightligh colors
 tfl = ['lblack', 'lred', 'lgreen', 'lyellow', 'lblue', 'lpink', 'lcyan', 'lwhite']
+
+# Background colors
 tb = ['blackb', 'redb', 'greenb', 'yellowb', 'blueb', 'pinkb', 'cyanb', 'whiteb']
 total = tf + tfl + tb
 
+# color code mapping
 cc = {
 	'black':'30m', 	'red':'31m', 	'green':'32m', 	'yellow':'33m',
 	'blue':'34m', 	'pink':'35m', 	'cyan':'36m', 	'white':'37m',
@@ -16,11 +20,14 @@ cc = {
 	'blueb':'44m', 'pinkb':'45m', 'cyanb':'46m', 'whiteb':'47m'
 	}
 
+# function code template
 code_temp = '''
 def %(func)s(s):
 	if s.endswith('\033[0m'): s = s[:-4]
 	return '\033[%(cc)s' + s + '\033[0m'
 '''
+
+# generate function on the fly
 for i in total: exec code_temp % {'func': i, 'cc': cc.get(i)} in globals()
 # for i in total: print code_temp % {'func': i, 'cc': cc.get(i)}
 
